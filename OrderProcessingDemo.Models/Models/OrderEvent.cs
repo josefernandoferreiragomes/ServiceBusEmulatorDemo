1720c5ef5abc1fa0c94326e61f9ae9a7a886e4d7
Jose Gomes <jose.fernando.ferreira.gomes@outlook.com>
2025-10-14 08:37:55 +0100

    add function app publisher

---
+++ OrderProcessingDemo.Models/Models/OrderEvent.cs
@@ -0,0 +1,9 @@
+﻿namespace OrderProcessingDemo.Models;
+
+public class OrderEvent
+{
+    public string OrderId { get; set; } = string.Empty;
+    public string CustomerId { get; set; } = string.Empty;
+    public decimal TotalPrice { get; set; } = 0;
+    public string ProductId { get; set; } = string.Empty;
+}
